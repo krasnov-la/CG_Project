0889208f63882c6504251f37ce63842b8ccbdbab
Le0Bonhart <Le0Bonhart@yandex.ru>
2023-03-21 12:29:59 +1000

    Point Vector

--- Point.cs
+++ Point.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,34 @@ using System.Threading.Tasks;
 
 namespace CG_Project
 {
-    internal class Point
+    class Point : DataTable
     {
+        public Point(int dim) : base(dim, 1) { }
+        
+        public Point(float[] data) : base(data.Length, 1)
+        {
+            for (int i = 0;  i < data.Length; i++)
+                SetElem(i, 0, data[i]);
+        }
+
+        public static Point operator +(Point point, Vector vector)
+        {
+            Point result = new Point(point.Rows);
+
+            for (int i = 0;i < point.Rows;i++)
+                result.SetElem(i, 0, point.GetElem(i, 0) + vector.GetElem(i, 0));
+            
+            return result;
+        }
+
+        public static Point operator -(Point point, Vector vector)
+        {
+            Point result = new Point(point.Rows);
+
+            for (int i = 0; i < point.Rows; i++)
+                result.SetElem(i, 0, point.GetElem(i, 0) - vector.GetElem(i, 0));
+
+            return result;
+        }
     }
 }
--- Vector.cs
+++ Vector.cs
@@ -7,7 +7,39 @@ using System.Threading.Tasks;
 
 namespace CG_Project
 {
-    class Vector
+    class Vector : Point
     {
+        public Vector(int dim) : base(dim) { }
+        
+        public Vector(float[] data) : base(data) { }
+
+        float ScalarProduct(Vector vector)
+        {
+            if (Rows != vector.Rows) return float.NaN;
+
+            float result = 0;
+
+            for (int i = 0; i < Rows; i++)
+                result += GetElem(i, 0) * vector.GetElem(i, 0);
+
+            return result; 
+        }
+
+        Vector VectorProduct(Vector vector)
+        {
+            Vector res = new Vector(vector.Rows);
+            return res;
+        }
+
+        float Lenght()
+        {
+            return (float)Math.Sqrt(ScalarProduct(this));
+        }
+
+        public static float operator %(Vector vector1, Vector vector2)
+            => vector1.ScalarProduct(vector2);
+
+        public static Vector operator ^(Vector vector1, Vector vector2)
+            => vector1.VectorProduct(vector2);
     }
 }
