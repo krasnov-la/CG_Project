f592c55192ebe70a08824450beaca25990379b1d
Le0Bonhart <Le0Bonhart@yandex.ru>
2023-06-08 01:06:23 +1000

    Added classes for console drawing

---
+++ lib/Engine/Visualisation/CamTypes/PlayerCamera.cs
@@ -0,0 +1,77 @@
+﻿using CGProject.Math;
+
+namespace CGProject.Engine
+{
+    public class PlayerCamera : GameCamera
+    {
+        float angle = 0;
+        public PlayerCamera(Game game, Point position, Vector direction, Tuple<float, float> FoV, float drawDist)
+            : base(game, position, direction, FoV, drawDist)
+        {
+            game.Mapper.Add(ConsoleKey.W, new Tuple<Entity, Action<Entity>>(this, MoveForward));
+            game.Mapper.Add(ConsoleKey.S, new Tuple<Entity, Action<Entity>>(this, MoveBackward));
+            game.Mapper.Add(ConsoleKey.A, new Tuple<Entity, Action<Entity>>(this, MoveLeft));
+            game.Mapper.Add(ConsoleKey.D, new Tuple<Entity, Action<Entity>>(this, MoveRight));
+            game.Mapper.Add(ConsoleKey.Spacebar, new Tuple<Entity, Action<Entity>>(this, MoveUp));
+            game.Mapper.Add(ConsoleKey.C, new Tuple<Entity, Action<Entity>>(this, MoveDown));
+            game.Mapper.Add(ConsoleKey.LeftArrow, new Tuple<Entity, Action<Entity>>(this, RotateLeft));
+            game.Mapper.Add(ConsoleKey.RightArrow, new Tuple<Entity, Action<Entity>>(this, RotateRight));
+            game.Mapper.Add(ConsoleKey.UpArrow, new Tuple<Entity, Action<Entity>>(this, RotateUp));
+            game.Mapper.Add(ConsoleKey.DownArrow, new Tuple<Entity, Action<Entity>>(this, RotateDown));
+        }
+
+        static void MoveUp(Entity ent)
+        {
+            PlayerCamera cam = (PlayerCamera)ent;
+            cam.Move(new Vector(0, 0, 0.1f));
+        }
+        static void MoveDown(Entity ent)
+        {
+            PlayerCamera cam = (PlayerCamera)ent;
+            cam.Move(new Vector(0, 0, -0.1f));
+        }
+        static void MoveForward(Entity ent)
+        {
+            PlayerCamera cam = (PlayerCamera) ent;
+            cam.Move(cam.CoordinateSystem.VS.Normalize(cam.Direction) / 10);
+        }
+        static void MoveLeft(Entity ent)
+        {
+            PlayerCamera cam = (PlayerCamera)ent;
+            throw new NotImplementedException();
+        }
+        static void MoveRight(Entity ent)
+        {
+            PlayerCamera cam = (PlayerCamera)ent;
+            throw new NotImplementedException();
+        }
+        static void MoveBackward(Entity ent)
+        {
+            PlayerCamera cam = (PlayerCamera)ent;
+            cam.Move(-1 * cam.CoordinateSystem.VS.Normalize(cam.Direction) / 10);
+        }
+
+        static void RotateLeft(Entity ent)
+        {
+            PlayerCamera cam = (PlayerCamera)ent;
+            cam.Rotate3D(0, 0, 10);
+        }
+
+        static void RotateRight(Entity ent)
+        {
+            PlayerCamera cam = (PlayerCamera)ent;
+            cam.Rotate3D(0, 0, -10);
+        }
+
+        static void RotateUp(Entity ent)
+        {
+            PlayerCamera cam = (PlayerCamera)ent;
+            throw new NotImplementedException();
+        }
+        static void RotateDown(Entity ent)
+        {
+            PlayerCamera cam = (PlayerCamera)ent;
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ lib/Engine/Visualisation/CanvasTypes/GameConsole.cs
@@ -0,0 +1,54 @@
+﻿using CGProject.Engine;
+using System.Text;
+using System.Xml.Linq;
+
+namespace CG_Project.Engine
+{
+    public class GameConsole : Canvas
+    {
+        static readonly string charMap = "@WNM0B$%&#D8XKAUbGOV496Pkqwfvszr*+<>;:.";
+
+        public GameConsole(Game game, int hor, int vert) : base(game, hor, vert) { }
+
+        public override void Draw(GameCamera cam)
+        {
+            Update(cam);
+            StringBuilder image = new StringBuilder();
+            for (int j = 0; j < _distances.GetLength(1); j++)
+            {
+                for (int i = 0; i < _distances.GetLength(0); i++)
+                    image.Append(GetChar(i, j, cam));
+
+                image.Append('\n');
+            }
+            Console.Clear();
+            Console.Write(image.ToString());
+        }
+
+        public override void Update(GameCamera cam)
+        {
+            Ray[,] rays = cam.GetRays(Hor, Vert);
+            for (int i = 0; i < Hor; i++)
+                for (int j = 0; j < Vert; j++)
+                {
+                    _distances[i, j] = null;
+                    foreach (GameObject obj in _game.Objects)
+                    {
+                        float? intersect = obj.IntersectionDist(rays[i, j]);
+                        if (intersect == null) continue;
+                        if (intersect > cam.DrawDist) continue;
+                        if (_distances[i, j] == null) _distances[i, j] = intersect;
+                        if (_distances[i, j] > intersect) _distances[i, j] = intersect;
+                    }
+                }
+        }
+
+        char GetChar(int i, int j, GameCamera cam)
+        {
+            if (_distances[i, j] == null) return charMap[charMap.Length - 1];
+            float ratio = (float)_distances[i, j] / cam.DrawDist;
+            int index = (int)(ratio * (charMap.Length - 1));
+            return charMap[index];
+        }
+    }
+}
