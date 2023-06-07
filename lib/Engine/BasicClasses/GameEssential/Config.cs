b82ad8ae4718b816138269d1d965ab943c6f634a
Le0Bonhart <Le0Bonhart@yandex.ru>
2023-06-08 01:08:17 +1000

    Json configs

---
+++ lib/Engine/BasicClasses/GameEssential/Config.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+namespace CGProject.Engine
+{
+    public class Configuration
+    {
+        public int HorizontalBlocks { get;set;}
+        public int VerticalBlocks { get;set;}
+        public float HorizontalFoV { get;set;}
+        public float VerticalFoV { get;set;}
+        public float DrawDist { get;set;}
+        public int FPS { get; set; }
+        public Configuration() { }      
+    }
+}
